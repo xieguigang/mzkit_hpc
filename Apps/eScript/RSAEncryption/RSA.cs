ecc886aa814f854a3fcff93c68fd02b63b7cdfe4
xieguigang <xie.guigang@gcmodeller.org>
2018-09-15 00:12:18 +0800

    add RSA helper

---
+++ Apps/eScript/RSAEncryption/RSA.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAEncryption
+{
+    public class RSA : IDisposable
+    {
+        // Members:
+        // RSA Key components (just the three I'm using, there is more...)
+        private BigInteger D = null;
+        private BigInteger Exponent = null;
+        private BigInteger Modulus = null;
+
+        // .NET RSA class, for loading and creating key pairs
+        private RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+
+        // flags, is the keys has been loaded yet?
+        private bool isPrivateKeyLoaded = false;
+        private bool isPublicKeyLoaded = false;
+
+        // Properties
+        public bool IsPrivateKeyLoaded
+        {
+            get
+            {
+                return isPrivateKeyLoaded;
+            }
+        }
+
+        public bool IsPublicKeyLoaded
+        {
+            get
+            {
+                return isPublicKeyLoaded;
+            }
+        }
+
+        // Methods:
+        public void LoadPublicFromXml(string publicKeyXmlString)
+        {
+            // Using the .NET RSA class to load a key from an Xml file, and populating the relevant members
+            // of my class with it's RSAParameters
+            try
+            {
+                rsa.FromXmlString(publicKeyXmlString);
+                RSAParameters rsaParams = rsa.ExportParameters(false);
+                Modulus = new BigInteger(rsaParams.Modulus);
+                Exponent = new BigInteger(rsaParams.Exponent);
+                isPublicKeyLoaded = true;
+                isPrivateKeyLoaded = false;
+            }
+            // Examle for the proper use of try - catch blocks: Informing the main app where and why the Exception occurred
+            catch (XmlSyntaxException ex)  // Not an xml file
+            {
+                string excReason = "Exception occurred at LoadPublicFromXml(), Selected file is not a valid xml file.";
+                throw new Exception(excReason, ex);
+            }
+            catch (CryptographicException ex)  // Not a Key file
+            {
+                string excReason = "Exception occurred at LoadPublicFromXml(), Selected xml file is not a public key file.";
+                throw new Exception(excReason, ex);
+            }
+            catch (Exception ex)  // other exception, hope the ex.message will help
+            {
+                string excReason = "General Exception occurred at LoadPublicFromXml().";
+                throw new Exception(excReason, ex);
+            }
+        }
+
+        // Same as the previous one, but this time loading the private Key
+        public void LoadPrivateFromXml(string privateKeyXmlString)
+        {
+            try
+            {
+                rsa.FromXmlString(privateKeyXmlString);
+                RSAParameters rsaParams = rsa.ExportParameters(true);
+                D = new BigInteger(rsaParams.D);  // This parameter is only for private key
+                Exponent = new BigInteger(rsaParams.Exponent);
+                Modulus = new BigInteger(rsaParams.Modulus);
+                isPrivateKeyLoaded = true;
+                isPublicKeyLoaded = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Encrypt data using private key
+        public byte[] PrivateEncryption(byte[] data)
+        {
+            if (!IsPrivateKeyLoaded)  // is the private key has been loaded?
+            {
+                throw new CryptographicException("Private Key must be loaded before using the Private Encryption method!");
+            }
+
+            string source = System.Text.Encoding.Default.GetString(data);
+
+            int len = source.Length;
+            int len1 = 0;
+            int blockLen = 0;
+            if ((len % 128) == 0)
+            {
+                len1 = len / 128;
+            }
+            else
+            {
+                len1 = len / 128 + 1;
+            }
+
+            string block = "";
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < len1; i++)
+            {
+                if (len >= 128)
+                {
+                    blockLen = 128;
+                }
+                else
+                {
+                    blockLen = len;
+                }
+
+                block = source.Substring(i * 128, blockLen);
+                byte[] oText = System.Text.Encoding.Default.GetBytes(block);
+                BigInteger biText = new BigInteger(oText);
+                BigInteger biEnText = biText.modPow(D, Modulus);
+                string temp = biEnText.ToHexString();
+                result.Append(temp).Append("@");
+                len -= blockLen;
+            }
+
+            return System.Text.Encoding.Default.GetBytes(result.ToString().TrimEnd('@'));
+        }
+
+        // Encrypt data using public key
+        public byte[] PublicEncryption(byte[] data)
+        {
+            if (!IsPublicKeyLoaded)  // is the public key has been loaded?
+            {
+                throw new CryptographicException("Public Key must be loaded before using the Public Encryption method!");
+            }
+
+            string source = System.Text.Encoding.Default.GetString(data);
+
+            int len = source.Length;
+            int len1 = 0;
+            int blockLen = 0;
+            if ((len % 128) == 0)
+            {
+                len1 = len / 128;
+            }
+            else
+            {
+                len1 = len / 128 + 1;
+            }
+            string block = "";
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < len1; i++)
+            {
+                if (len >= 128)
+                {
+                    blockLen = 128;
+                }
+                else
+                {
+                    blockLen = len;
+                }
+                block = source.Substring(i * 128, blockLen);
+                byte[] oText = System.Text.Encoding.Default.GetBytes(block);
+                BigInteger biText = new BigInteger(oText);
+                BigInteger biEnText = biText.modPow(Exponent, Modulus);
+                string temp = biEnText.ToHexString();
+                result.Append(temp).Append("@");
+                len -= blockLen;
+            }
+
+            return System.Text.Encoding.Default.GetBytes(result.ToString().TrimEnd('@'));
+        }
+
+        // Decrypt data using private key (for data encrypted with public key)
+        public byte[] PrivateDecryption(byte[] encryptedData)
+        {
+            if (!IsPrivateKeyLoaded)  // is the private key has been loaded?
+            {
+                throw new CryptographicException("Private Key must be loaded before using the Private Decryption method!");
+            }
+
+            string encryptString = System.Text.Encoding.Default.GetString(encryptedData);
+
+            StringBuilder result = new StringBuilder();
+            string[] strarr1 = encryptString.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < strarr1.Length; i++)
+            {
+                string block = strarr1[i];
+                BigInteger biText = new BigInteger(block, 16);
+                BigInteger biEnText = biText.modPow(D, Modulus);
+                string temp = System.Text.Encoding.Default.GetString(biEnText.getBytes());
+                result.Append(temp);
+            }
+            return System.Text.Encoding.Default.GetBytes(result.ToString());
+        }
+
+        // Decrypt data using public key (for data encrypted with private key)
+        public byte[] PublicDecryption(byte[] encryptedData)
+        {
+            if (!IsPublicKeyLoaded)  // is the public key has been loaded?
+            {
+                throw new CryptographicException("Public Key must be loaded before using the Public Deccryption method!");
+            }
+
+
+            string encryptString = System.Text.Encoding.Default.GetString(encryptedData);
+
+            StringBuilder result = new StringBuilder();
+            string[] strarr1 = encryptString.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < strarr1.Length; i++)
+            {
+                string block = strarr1[i];
+                BigInteger biText = new BigInteger(block, 16);
+                BigInteger biEnText = biText.modPow(Exponent, Modulus);
+                string temp = System.Text.Encoding.Default.GetString(biEnText.getBytes());
+                result.Append(temp);
+            }
+            return System.Text.Encoding.Default.GetBytes(result.ToString());
+        }
+
+        // Implementation of IDisposable interface,
+        // allow you to use this class as: using(RSAEncryption rsa = new RSAEncryption()) { ... }
+        public void Dispose()
+        {
+            rsa.Clear();
+        }
+    }
+}
