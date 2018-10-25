b05aebff2bd20573432fed470407c90b1dd8a320
xieguigang <xie.guigang@gcmodeller.org>
2018-10-25 22:51:54 +0800

    cmd

---
+++ Apps/CMD/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports Microsoft.VisualBasic.Language
+
+Public Class Form1
+
+    Dim commandBuffer As New List(Of Char)
+    Dim chars As New Dictionary(Of Keys, Char)
+
+    Private Sub boxTerminal_KeyDown(sender As Object, e As KeyEventArgs) Handles boxTerminal.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If commandBuffer > 0 Then
+                Call FireCommand(commandBuffer.CharString)
+                Call commandBuffer.Clear()
+            End If
+
+            Call boxTerminal.AppendText("# ")
+        ElseIf chars.ContainsKey(Keys.KeyCode) Then
+            commandBuffer += chars(e.KeyCode)
+        End If
+    End Sub
+
+    Private Sub FireCommand(cmd As String)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        chars(Keys.A) = "a"
+        chars(Keys.B) = "b"
+        chars(Keys.C) = "c"
+        chars(Keys.D) = "d"
+        chars(Keys.E) = "e"
+        chars(Keys.F) = "f"
+        chars(Keys.G) = "g"
+        chars(Keys.H) = "h"
+        chars(Keys.I) = "i"
+        chars(Keys.J) = "j"
+        chars(Keys.K) = "k"
+        chars(Keys.L) = "l"
+        chars(Keys.M) = "m"
+        chars(Keys.N) = "n"
+        chars(Keys.O) = "o"
+        chars(Keys.P) = "p"
+        chars(Keys.Q) = "q"
+        chars(Keys.R) = "r"
+        chars(Keys.S) = "s"
+        chars(Keys.T) = "t"
+        chars(Keys.U) = "u"
+        chars(Keys.V) = "v"
+        chars(Keys.W) = "w"
+        chars(Keys.X) = "x"
+        chars(Keys.Y) = "y"
+        chars(Keys.Z) = "z"
+        chars(Keys.D0) = "0" : chars(Keys.NumPad0) = "0"
+        chars(Keys.D0) = "1" : chars(Keys.NumPad0) = "1"
+        chars(Keys.D0) = "2" : chars(Keys.NumPad0) = "2"
+        chars(Keys.D0) = "3" : chars(Keys.NumPad0) = "3"
+        chars(Keys.D0) = "4" : chars(Keys.NumPad0) = "4"
+        chars(Keys.D0) = "5" : chars(Keys.NumPad0) = "5"
+        chars(Keys.D0) = "6" : chars(Keys.NumPad0) = "6"
+        chars(Keys.D0) = "7" : chars(Keys.NumPad0) = "7"
+        chars(Keys.D0) = "8" : chars(Keys.NumPad0) = "8"
+        chars(Keys.D0) = "9" : chars(Keys.NumPad0) = "9"
+    End Sub
+End Class
--- Apps/eScript/Program.vb
+++ Apps/eScript/Program.vb
@@ -1,7 +1,35 @@
-﻿Module Program
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
-    Sub Main()
+Module Program
 
-    End Sub
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
 
+    <ExportAPI("/rsa.key")>
+    <Usage("/rsa.key [/out <default=./>]")>
+    Public Function CreateRSAKeys(args As CommandLine) As Integer
+        Dim rsa As RSACryptoServiceProvider = New RSACryptoServiceProvider
+        Dim dir As String = args("/out") Or "./"
+
+        Using writer As StreamWriter = $"{dir}/PrivateKey.xml".OpenWriter
+            ' 这个文件是需要进行保密的密匙
+            writer.WriteLine(rsa.ToXmlString(True))
+        End Using
+        Using writer As StreamWriter = $"{dir}/PublicKey.xml".OpenWriter
+            ' 这个是分发给其他人的公匙
+            writer.WriteLine(rsa.ToXmlString(False))
+        End Using
+
+        Return 0
+    End Function
+
+    <ExportAPI("/rsa.encrypt")>
+    <Usage("/rsa.encrypt /message <text> /private <privatekey.xml> [/out <out.txt>]")>
+    Public Function PrivateEncrypt(args As CommandLine) As Integer
+
+    End Function
 End Module
