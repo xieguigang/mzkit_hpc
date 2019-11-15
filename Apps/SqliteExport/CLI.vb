34f74c247aadf36d84a13f30108f53032d934174
xieguigang <xie.guigang@gcmodeller.org>
2019-11-15 09:48:29 +0800

    require a sql statement string parser

--- Apps/SqliteExport/CLI.vb
+++ Apps/SqliteExport/CLI.vb
@@ -1,3 +1,25 @@
-﻿Module CLI
+﻿Imports System.Data.SQLite
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
+Module CLI
+
+    <ExportAPI("/export")>
+    <Usage("/export /db <database.sqlite3> /table <tableName> [/out <table.csv>]")>
+    Public Function exportTable(args As CommandLine) As Integer
+        Dim dbFile$ = args <= "/db"
+        Dim tableName$ = args <= "/table"
+        Dim out$ = args("/out") Or $"{dbFile.TrimSuffix}_{tableName}.csv"
+        Dim fields As String()
+
+        Call Program.SelectQueryTable(
+            dbFile, "sqlite_master",
+            Function(names)
+                ' CREATE TABLE sqlite_master (type TEXT, name TEXT, tbl_name TEXT, rootpage INTEGER, sql TEXT);
+                If CStr(names(Scan0)) = "table" AndAlso CStr(names(1)) Then
+
+                End If
+            End Function)
+
+    End Function
 End Module
--- Apps/SqliteExport/Program.vb
+++ Apps/SqliteExport/Program.vb
@@ -1,7 +1,32 @@
-﻿Module Program
+﻿Imports System.Data.SQLite
+
+Module Program
 
     Public Function Main() As Integer
         Return GetType(CLI).RunCLI(App.CommandLine)
     End Function
+
+    Public Sub SelectQueryTable(dbFile$, tableName$, rowAction As Func(Of Object(), Boolean))
+        Dim connStr$ = $"data source={dbFile.GetFullPath};version=3;"
+        Dim cn As New SQLiteConnection(connStr)
+        Call cn.Open()
+
+        Dim cmd = cn.CreateCommand()
+        cmd.CommandText = $"SELECT * FROM {tableName};"
+
+        Dim row As New List(Of Object)
+
+        Using reader As SQLiteDataReader = cmd.ExecuteReader()
+            Do While reader.Read()
+                For i As Integer = 0 To reader.FieldCount - 1
+                    Call row.Add(reader(i))
+                Next
+
+                If rowAction(row.ToArray) Then
+                    Exit Do
+                End If
+            Loop
+        End Using
+    End Sub
 
 End Module
