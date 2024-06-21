122e03eb7f2b683e073815728fddb16552510441
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-21 20:38:47 +0800

    make error fixed of the file read/load

--- src/IpcParallel/DoStatMatrix.vb
+++ src/IpcParallel/DoStatMatrix.vb
@@ -1,18 +1,13 @@
+Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports batch
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells.Deconvolute
-Imports batch
-Imports Darwinism.HPC.Parallel
-Imports Darwinism.HPC.Parallel.IpcStream
-Imports Microsoft.VisualBasic.Data.GraphTheory.KdTree.ApproximateNearNeighbor
-Imports Microsoft.VisualBasic.Data.GraphTheory.KNearNeighbors
-Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 Imports Darwinism.DataScience.DataMining
-Imports System.IO
+Imports Darwinism.HPC.Parallel
 Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text
-Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ASCII.MGF
 
 Public Module DoStatMatrix
 
@@ -22,18 +17,14 @@ Public Module DoStatMatrix
             .Select(Function(si) si.label) _
             .ToArray
         Dim env As Argument = DarwinismEnvironment.GetEnvironmentArguments
-        Dim vectorPack = matrix.getFeatures _
-            .Split(CInt(matrix.featureSize / env.n_threads / 2)) _
-            .Select(Function(part)
-                        Return New FeatureVectorPack With {
-                            .cell_labels = labels,
-                            .ions = part
-                        }
-                    End Function) _
-            .ToArray
-        Dim pool As SocketRef = SocketRef.WriteBuffer(vectorPack, StreamEmit.Custom(Of FeatureVectorPack())(New FeatureVectorPackFile))
+        Dim vectorPack = matrix.getFeatures.Split(CInt(matrix.featureSize / env.n_threads / 2))
+        Dim task As New Func(Of FeatureVector(), SingleCellIonStat())(AddressOf MeasureIonsFeaturesTask)
 
-
+        For Each batch As SingleCellIonStat() In Host.ParallelFor(Of FeatureVector(), SingleCellIonStat())(env, task, vectorPack)
+            For Each ion_stat As SingleCellIonStat In batch
+                Yield ion_stat
+            Next
+        Next
     End Function
 
     <Extension>
@@ -55,8 +46,8 @@ Public Module DoStatMatrix
         Next
     End Function
 
-    <EmitStream(GetType(FeatureVectorPackFile), Target:=GetType(FeatureVectorPack()))>
-    Public Function MeasureIonsFeatures(packs As FeatureVectorPack()) As SingleCellIonStat()
+    <EmitStream(GetType(FeatureVectorPackFile), Target:=GetType(FeatureVector()))>
+    Public Function MeasureIonsFeaturesTask(packs As FeatureVector()) As SingleCellIonStat()
 
     End Function
 
@@ -69,76 +60,65 @@ Public Class FeatureVectorPackFile : Implements IEmitStream
     End Function
 
     Public Function WriteBuffer(obj As Object, file As Stream) As Boolean Implements IEmitStream.WriteBuffer
-        Call FeatureVectorPack.Save(DirectCast(obj, FeatureVectorPack()), file)
+        Call Save(DirectCast(obj, FeatureVector()), file)
         Call file.Flush()
         Return True
     End Function
 
     Public Function WriteBuffer(obj As Object) As Stream Implements IEmitStream.WriteBuffer
         Dim file As New MemoryStream
-        Call FeatureVectorPack.Save(DirectCast(obj, FeatureVectorPack()), file)
+        Call Save(DirectCast(obj, FeatureVector()), file)
         Call file.Flush()
         Call file.Seek(0, SeekOrigin.Begin)
         Return file
     End Function
 
     Public Function ReadBuffer(file As Stream) As Object Implements IEmitStream.ReadBuffer
-        Return FeatureVectorPack.Load(file).ToArray
+        Return Load(file).ToArray
     End Function
-End Class
 
-Public Class FeatureVectorPack
-
-    Public Property cell_labels As String()
-    Public Property ions As FeatureVector()
-
-    Public Shared Sub Save(pack As FeatureVectorPack(), file As Stream)
+    Public Shared Sub Save(pack As FeatureVector(), file As Stream)
         Dim bin As New BinaryDataWriter(file, Encodings.ASCII)
 
         ' flag n packages
         Call bin.Write(pack.Length)
 
-        For Each vec As FeatureVectorPack In pack
-            Call bin.Write(vec.ions.Length)
+        For Each vec As FeatureVector In pack
             Call bin.Write(vec.cell_labels.Length)
+            Call bin.Write(vec.mz)
+            Call bin.Write(vec.mzmin)
+            Call bin.Write(vec.mzmax)
+            Call bin.Write(vec.intensity)
 
             For Each str As String In vec.cell_labels
                 Call bin.Write(str, BinaryStringFormat.DwordLengthPrefix)
-            Next
-            For Each ion As FeatureVector In vec.ions
-                Call bin.Write(ion.mz)
-                Call bin.Write(ion.mzmin)
-                Call bin.Write(ion.mzmax)
-                Call bin.Write(ion.intensity)
             Next
         Next
 
         Call bin.Flush()
     End Sub
 
-    Public Shared Iterator Function Load(file As Stream) As IEnumerable(Of FeatureVectorPack)
+    Public Shared Iterator Function Load(file As Stream) As IEnumerable(Of FeatureVector)
         Dim bin As New BinaryDataReader(file, Encodings.ASCII)
         Dim n As Integer = bin.ReadInt32
 
         For null As Integer = 1 To n
-            Dim ions As FeatureVector() = New FeatureVector(bin.ReadInt32 - 1) {}
             Dim labels As String() = New String(bin.ReadInt32 - 1) {}
+            Dim mz As Double = bin.ReadDouble
+            Dim mzmin As Double = bin.ReadDouble
+            Dim mzmax As Double = bin.ReadDouble
+            Dim into As Double() = bin.ReadDoubles(labels.Length)
 
             For i As Integer = 0 To labels.Length - 1
                 labels(i) = bin.ReadString(BinaryStringFormat.DwordLengthPrefix)
             Next
-            For i As Integer = 0 To ions.Length - 1
-                ions(i) = New FeatureVector With {
-                    .mz = bin.ReadDouble,
-                    .mzmin = bin.ReadDouble,
-                    .mzmax = bin.ReadDouble,
-                    .intensity = bin.ReadDoubles(labels.Length)
-                }
-            Next
 
-            Yield New FeatureVectorPack With {
+            Yield New FeatureVector With {
                 .cell_labels = labels,
-                .ions = ions
+                .mz = mz,
+                .mzmin = mzmin,
+                .mzmax = mzmax,
+                .intensity = into
             }
         Next
     End Function
@@ -147,6 +127,7 @@ End Class
 
 Public Class FeatureVector
 
+    Public Property cell_labels As String()
     Public Property mz As Double
     Public Property mzmin As Double
     Public Property mzmax As Double
