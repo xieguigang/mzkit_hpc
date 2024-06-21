4bc3fb926c8907c422001a25464badf20fe15c3a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-21 18:56:48 +0800

    run package msbuild

---
+++ src/hpc/MsImaging.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' MS-imaging rawdata processing in HPC parallel
+''' </summary>
+<Package("MsImaging")>
+Public Module MsImaging
+
+    Sub Main()
+
+    End Sub
+End Module
---
+++ src/hpc/zzz.vb
@@ -0,0 +1,10 @@
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Assembly: RPackageModule>
+
+Public Class zzz
+
+    Public Shared Sub onLoad()
+        Call MsImaging.Main()
+    End Sub
+End Class
