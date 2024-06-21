9966c0b698b362757b1c03dddeb2298018c3e5f1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-21 21:36:14 +0800

    measure ion feature for MSI

--- src/IpcParallel/FeatureVector.vb
+++ src/IpcParallel/FeatureVector.vb
@@ -1,11 +1,15 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells
+Imports Microsoft.VisualBasic.Data.GraphTheory.GridGraph
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Information
 Imports Microsoft.VisualBasic.Math.Quantile
 Imports Microsoft.VisualBasic.Math.SIMD
+Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis
+Imports Microsoft.VisualBasic.Scripting.Runtime
+Imports Point = System.Drawing.Point
 
 Public Class FeatureVector
 
@@ -16,7 +20,79 @@ Public Class FeatureVector
     Public Property intensity As Double()
 
     Public Function MeasureMSIIon(grid_size As Integer) As IonStat
+        Dim mean As Double = intensity.Average
+        Dim max_i As Integer = which.Max(intensity)
+        Dim maxo As Integer = intensity(max_i)
+        Dim max_spot As Integer() = cell_labels(max_i).Split(","c).AsInteger
+        Dim spots As (xyz As String, intensity As Double)() = (
+            From cell As (String, Double)
+            In cell_labels.Zip(intensity)
+            Where cell.Second > 0).ToArray
+        Dim points = spots.Select(Function(cell) cell.xyz.Split(","c).AsInteger).ToArray
+        Dim pixels = points.Select(Function(a) New Point(a(0), a(1))).CreateReadOnlyGrid
+        Dim moran_test As MoranTest
+        Dim quartile As DataQuartile = intensity.Quartile
 
+        If points.Length < 3 Then
+            moran_test = New MoranTest With {
+                .df = 0, .Expected = 0, .Observed = 0,
+                .prob2 = 1, .pvalue = 1, .SD = 0,
+                .t = 0, .z = 0
+            }
+        Else
+            moran_test = MoranTest.moran_test(
+                x:=spots.Select(Function(c) c.intensity).ToArray,
+                c1:=points.Select(Function(p) CDbl(p(0))).ToArray,
+                c2:=points.Select(Function(p) CDbl(p(1))).ToArray,
+                parallel:=False,
+                throwMaxIterError:=False
+            )
+        End If
+
+        Dim counts As New List(Of Double)
+        Dim density As Double = 0
+
+        For Each top As (xyz As String, intensity As Double) In (
+            From cell
+            In spots
+            Order By cell.intensity Descending
+            Take 30
+        )
+            Dim spatial As Integer() = top.xyz.Split(","c).AsInteger
+
+            Call counts.Add(Aggregate cell As Point
+                            In pixels.Query(spatial(0), spatial(1), grid_size)
+                            Where Not cell.IsEmpty
+                            Into Count)
+        Next
+
+        If counts.Count > 0 Then
+            density = SIMD.Divide.f64_op_divide_f64_scalar(counts.ToArray, grid_size ^ 2).Average
+        End If
+
+        Dim total_cells As Integer = intensity.Length
+        Dim intensity_vec = Divide.f64_op_divide_f64_scalar(intensity, intensity.Sum)
+
+        Return New IonStat With {
+            .averageIntensity = mean,
+            .basePixelX = max_spot(0),
+            .basePixelY = max_spot(1),
+            .density = density,
+            .maxIntensity = maxo,
+            .moran = moran_test.Observed,
+            .mz = mz,
+            .mzmin = mzmin,
+            .mzmax = mzmax,
+            .mzwidth = MassWindow.ToString(.mzmax, .mzmin),
+            .pixels = spots.Length,
+            .pvalue = moran_test.pvalue,
+            .Q1Intensity = quartile.Q1,
+            .Q2Intensity = quartile.Q2,
+            .Q3Intensity = quartile.Q3,
+            .entropy = intensity_vec.ShannonEntropy,
+            .rsd = intensity_vec.RSD,
+            .sparsity = 1 - .pixels / total_cells
+        }
     End Function
 
     Public Function MeasureStat() As SingleCellIonStat
@@ -44,7 +120,7 @@ Public Class FeatureVector
             .mz = mz,
             .mzmin = mzmin,
             .mzmax = mzmax,
-            .mz_error = MassWindow.ToString(.mzmin, .mzmax)
+            .mz_error = MassWindow.ToString(.mzmax, .mzmin)
         }
     End Function
 End Class
