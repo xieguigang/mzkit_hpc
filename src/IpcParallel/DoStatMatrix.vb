f518790becccd81e19586ef946326b28968ff991
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-21 19:55:08 +0800

    create parallel framework

--- src/IpcParallel/DoStatMatrix.vb
+++ src/IpcParallel/DoStatMatrix.vb
@@ -1,12 +1,95 @@
 Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells.Deconvolute
+Imports batch
+Imports Darwinism.HPC.Parallel
+Imports Darwinism.HPC.Parallel.IpcStream
+Imports Microsoft.VisualBasic.Data.GraphTheory.KdTree.ApproximateNearNeighbor
+Imports Microsoft.VisualBasic.Data.GraphTheory.KNearNeighbors
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+Imports Darwinism.DataScience.DataMining
+Imports System.IO
 
 Public Module DoStatMatrix
 
     <Extension>
     Public Iterator Function MeasureIonFeatures(matrix As MzMatrix) As IEnumerable(Of SingleCellIonStat)
+        Dim labels As String() = matrix.matrix _
+            .Select(Function(si) si.label) _
+            .ToArray
+        Dim env As Argument = DarwinismEnvironment.GetEnvironmentArguments
+        Dim vectorPack = matrix.getFeatures _
+            .Split(CInt(matrix.featureSize / env.n_threads / 2)) _
+            .Select(Function(part)
+                        Return New FeatureVectorPack With {
+                            .cell_labels = labels,
+                            .ions = part
+                        }
+                    End Function) _
+            .ToArray
+        Dim pool As SocketRef = SocketRef.WriteBuffer(vectorPack, StreamEmit.Custom(Of FeatureVectorPack())(New FeatureVectorPackFile))
+
+
+    End Function
+
+    <Extension>
+    Private Iterator Function getFeatures(matrix As MzMatrix) As IEnumerable(Of FeatureVector)
+        Dim offset As Integer
+        Dim mat As PixelData() = matrix.matrix
+
+        For i As Integer = 0 To matrix.featureSize - 1
+            offset = i
+
+            Yield New FeatureVector With {
+                .mz = matrix.mz(i),
+                .mzmin = matrix.mzmin(i),
+                .mzmax = matrix.mzmax(i),
+                .intensity = (From cell As PixelData
+                              In mat
+                              Select cell(offset)).ToArray
+            }
+        Next
+    End Function
+
+    <EmitStream(GetType(FeatureVectorPackFile), Target:=GetType(FeatureVectorPack()))>
+    Public Function MeasureIonsFeatures(packs As FeatureVectorPack()) As SingleCellIonStat()
 
     End Function
 
 End Module
+
+Public Class FeatureVectorPackFile : Implements IEmitStream
+
+    Public Function BufferInMemory(obj As Object) As Boolean Implements IEmitStream.BufferInMemory
+        Return True
+    End Function
+
+    Public Function WriteBuffer(obj As Object, file As Stream) As Boolean Implements IEmitStream.WriteBuffer
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function WriteBuffer(obj As Object) As Stream Implements IEmitStream.WriteBuffer
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function ReadBuffer(file As Stream) As Object Implements IEmitStream.ReadBuffer
+        Throw New NotImplementedException()
+    End Function
+End Class
+
+Public Class FeatureVectorPack
+
+    Public Property cell_labels As String()
+    Public Property ions As FeatureVector()
+
+End Class
+
+Public Class FeatureVector
+
+    Public Property mz As Double
+    Public Property mzmin As Double
+    Public Property mzmax As Double
+    Public Property intensity As Double()
+
+End Class
