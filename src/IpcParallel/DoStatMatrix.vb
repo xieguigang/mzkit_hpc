9a99f2e901c69ba3ede787829e01f29211a5cb90
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-21 20:21:55 +0800

    save/load of the data pack

--- src/IpcParallel/DoStatMatrix.vb
+++ src/IpcParallel/DoStatMatrix.vb
@@ -10,6 +10,9 @@ Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 Imports Darwinism.DataScience.DataMining
 Imports System.IO
+Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Text
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.ASCII.MGF
 
 Public Module DoStatMatrix
 
@@ -66,15 +69,21 @@ Public Class FeatureVectorPackFile : Implements IEmitStream
     End Function
 
     Public Function WriteBuffer(obj As Object, file As Stream) As Boolean Implements IEmitStream.WriteBuffer
-        Throw New NotImplementedException()
+        Call FeatureVectorPack.Save(DirectCast(obj, FeatureVectorPack()), file)
+        Call file.Flush()
+        Return True
     End Function
 
     Public Function WriteBuffer(obj As Object) As Stream Implements IEmitStream.WriteBuffer
-        Throw New NotImplementedException()
+        Dim file As New MemoryStream
+        Call FeatureVectorPack.Save(DirectCast(obj, FeatureVectorPack()), file)
+        Call file.Flush()
+        Call file.Seek(0, SeekOrigin.Begin)
+        Return file
     End Function
 
     Public Function ReadBuffer(file As Stream) As Object Implements IEmitStream.ReadBuffer
-        Throw New NotImplementedException()
+        Return FeatureVectorPack.Load(file).ToArray
     End Function
 End Class
 
@@ -82,6 +91,57 @@ Public Class FeatureVectorPack
 
     Public Property cell_labels As String()
     Public Property ions As FeatureVector()
+
+    Public Shared Sub Save(pack As FeatureVectorPack(), file As Stream)
+        Dim bin As New BinaryDataWriter(file, Encodings.ASCII)
+
+        ' flag n packages
+        Call bin.Write(pack.Length)
+
+        For Each vec As FeatureVectorPack In pack
+            Call bin.Write(vec.ions.Length)
+            Call bin.Write(vec.cell_labels.Length)
+
+            For Each str As String In vec.cell_labels
+                Call bin.Write(str, BinaryStringFormat.DwordLengthPrefix)
+            Next
+            For Each ion As FeatureVector In vec.ions
+                Call bin.Write(ion.mz)
+                Call bin.Write(ion.mzmin)
+                Call bin.Write(ion.mzmax)
+                Call bin.Write(ion.intensity)
+            Next
+        Next
+
+        Call bin.Flush()
+    End Sub
+
+    Public Shared Iterator Function Load(file As Stream) As IEnumerable(Of FeatureVectorPack)
+        Dim bin As New BinaryDataReader(file, Encodings.ASCII)
+        Dim n As Integer = bin.ReadInt32
+
+        For null As Integer = 1 To n
+            Dim ions As FeatureVector() = New FeatureVector(bin.ReadInt32 - 1) {}
+            Dim labels As String() = New String(bin.ReadInt32 - 1) {}
+
+            For i As Integer = 0 To labels.Length - 1
+                labels(i) = bin.ReadString(BinaryStringFormat.DwordLengthPrefix)
+            Next
+            For i As Integer = 0 To ions.Length - 1
+                ions(i) = New FeatureVector With {
+                    .mz = bin.ReadDouble,
+                    .mzmin = bin.ReadDouble,
+                    .mzmax = bin.ReadDouble,
+                    .intensity = bin.ReadDoubles(labels.Length)
+                }
+            Next
+
+            Yield New FeatureVectorPack With {
+                .cell_labels = labels,
+                .ions = ions
+            }
+        Next
+    End Function
 
 End Class
 
