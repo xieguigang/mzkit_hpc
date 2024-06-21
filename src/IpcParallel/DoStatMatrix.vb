a3f8107a00fe7f785c478edf29b6a978e9d11a75
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-21 20:46:08 +0800

    create parallel task for single cells raw data matrix

--- src/IpcParallel/DoStatMatrix.vb
+++ src/IpcParallel/DoStatMatrix.vb
@@ -1,12 +1,17 @@
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports batch
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells
 Imports BioNovoGene.Analytical.MassSpectrometry.SingleCells.Deconvolute
 Imports Darwinism.DataScience.DataMining
 Imports Darwinism.HPC.Parallel
 Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.Information
+Imports Microsoft.VisualBasic.Math.Quantile
+Imports Microsoft.VisualBasic.Math.SIMD
 Imports Microsoft.VisualBasic.Text
 
 Public Module DoStatMatrix
@@ -48,7 +53,7 @@ Public Module DoStatMatrix
 
     <EmitStream(GetType(FeatureVectorPackFile), Target:=GetType(FeatureVector()))>
     Public Function MeasureIonsFeaturesTask(packs As FeatureVector()) As SingleCellIonStat()
-
+        Return (From i As FeatureVector In packs Select i.MeasureStat).ToArray
     End Function
 
 End Module
@@ -133,4 +138,32 @@ Public Class FeatureVector
     Public Property mzmax As Double
     Public Property intensity As Double()
 
+    Public Function MeasureStat() As SingleCellIonStat
+        Dim max_i As Integer = which.Max(intensity)
+        Dim counts As Integer = Aggregate xi As Double
+                                In intensity
+                                Where xi > 0
+                                Into Count
+        Dim quartile As DataQuartile = intensity.Quartile
+        Dim base_cell = cell_labels(max_i)
+        Dim maxo As Double = intensity(max_i)
+        Dim intensity_vec = Divide.f64_op_divide_f64_scalar(intensity, intensity.Sum)
+        Dim total_cells As Integer = intensity.Length
+
+        Return New SingleCellIonStat With {
+            .baseCell = base_cell,
+            .cells = counts,
+            .maxIntensity = maxo,
+            .Q1Intensity = quartile.Q1,
+            .Q2Intensity = quartile.Q2,
+            .Q3Intensity = quartile.Q3,
+            .entropy = intensity_vec.ShannonEntropy,
+            .RSD = intensity_vec.RSD,
+            .sparsity = 1 - counts / total_cells,
+            .mz = mz,
+            .mzmin = mzmin,
+            .mzmax = mzmax,
+            .mz_error = MassWindow.ToString(.mzmin, .mzmax)
+        }
+    End Function
 End Class
