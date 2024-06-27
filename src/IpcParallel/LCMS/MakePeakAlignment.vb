47b5b2ec8540deea0e7aaf4def73b8bd3210aee5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-27 15:17:56 +0800

    move algorithm workflow module into base project

--- src/IpcParallel/LCMS/MakePeakAlignment.vb
+++ src/IpcParallel/LCMS/MakePeakAlignment.vb
@@ -1,11 +1,17 @@
-﻿Imports System.Runtime.CompilerServices
-Imports BioNovoGene.Analytical.MassSpectrometry.Math
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.MIME.application.json
 
-Public Module MakePeakAlignment
+Public Class MakePeakAlignment
 
-    <Extension>
+    Public Property features_mz As Double()
+    Public Property errors As String
+    Public Property rt_range As Double()
+    Public Property baseline As Double
+    Public Property joint As Boolean
+    Public Property dtw As Boolean
+
     Public Function CreatePeaktable(pool As XICPool, features_mz As Double(),
                                     errors As Tolerance,
                                     rtRange As DoubleRange,
@@ -13,5 +19,15 @@ Public Module MakePeakAlignment
                                     joint As Boolean,
                                     dtw As Boolean, ByRef rt_shifts As RtShift()) As xcms2()
 
+        Dim pack As String = New MakePeakAlignment With {
+            .baseline = baseline,
+            .dtw = dtw,
+            .errors = errors.ToScript,
+            .features_mz = features_mz,
+            .joint = joint,
+            .rt_range = rtRange.MinMax
+        }.GetJson
+
     End Function
-End Module
+
+End Class
