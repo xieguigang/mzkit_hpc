7360ca82cc54e322bb97a515c4651c6f54b295bc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-27 12:49:40 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@ imports "MsImaging" from "hpc";
 
 const .onLoad = function() {
     print(["Build high performance computing mzkit algorithm library based on Darwinism HPC runtime", 
-           "an engineering practice of processing ultra massive lc-ms mass spectrometry rawdata, ms-imaging rawdata and single-cells metabolomics rawdata."
+           "An engineering practice of processing ultra massive lc-ms mass spectrometry rawdata, ms-imaging rawdata and single-cells metabolomics rawdata."
     ]);
 }
