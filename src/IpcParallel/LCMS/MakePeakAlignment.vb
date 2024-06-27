e586cb1b9c9827b1ab9054f307a1af21e61adf18
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-27 15:33:37 +0800

    define parallel task

--- src/IpcParallel/LCMS/MakePeakAlignment.vb
+++ src/IpcParallel/LCMS/MakePeakAlignment.vb
@@ -1,5 +1,6 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.MIME.application.json
 
@@ -11,7 +12,7 @@ Public Class MakePeakAlignment
     Public Property joint As Boolean
     Public Property dtw As Boolean
 
-    Public Function CreatePeaktable(pool As XICPool, features_mz As Double(),
+    Public Function CreatePeaktable(peak_groups As XICPool, features_mz As Double(),
                                     errors As Tolerance,
                                     rtRange As DoubleRange,
                                     baseline As Double,
@@ -25,7 +26,80 @@ Public Class MakePeakAlignment
             .joint = joint,
             .rt_range = rtRange.MinMax
         }.GetJson
+        Dim pool As FeatureXic() = features_mz.AsParallel _
+            .Select(Function(mz)
+                        Dim pick_features = peak_groups.GetXICMatrix(mz, errors).ToArray
+                        Dim sources As String() = pick_features.Select(Function(a) a.Name).ToArray
+                        Dim peaks As MzGroup() = pick_features.Select(Function(a) a.Value).ToArray
 
+                        Return New FeatureXic With {
+                            .mz = mz,
+                            .peak_groups = peaks,
+                            .samples = sources
+                        }
+                    End Function) _
+            .ToArray
+
+    End Function
+
+    Private Shared Function MakePeakTable(peak_groups As FeatureXic(), args As MakePeakAlignment) As PeakTablePack
+        Dim peaktable As New List(Of xcms2)
+        Dim rt_shifts As New List(Of RtShift)
+
+        For Each peak As FeatureXic In peak_groups
+            With peak.GetPeakTable(args)
+                Call peaktable.AddRange(.peaks)
+                Call rt_shifts.AddRange(.rt_shifts)
+            End With
+        Next
+
+        Return New PeakTablePack With {
+            .peaktable = peaktable.ToArray,
+            .rt_shifts = rt_shifts.ToArray
+        }
+    End Function
+
+End Class
+
+Public Class PeakTablePack
+
+    Public Property peaktable As xcms2()
+    Public Property rt_shifts As RtShift()
+
+End Class
+
+Public Class FeatureXic
+
+    Public Property mz As Double
+    Public Property samples As String()
+    ''' <summary>
+    ''' the xic features vector is keeps the same dimension size with the 
+    ''' <see cref="samples"/> character vector, each sample name has a 
+    ''' corresponding xic feature data from this vector in the same index
+    ''' offset position.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property peak_groups As MzGroup()
+
+    Public Function GetPeakTable(pars As MakePeakAlignment) As (peaks As xcms2(), rt_shifts As RtShift())
+        Dim samples_xic = samples _
+            .Select(Function(name, i)
+                        Return New NamedValue(Of MzGroup)(name, peak_groups(i))
+                    End Function) _
+            .ToArray
+        Dim shifts As New List(Of RtShift)
+
+        If pars.dtw Then
+            samples_xic = XICPool.DtwXIC(samples_xic).ToArray
+        End If
+
+        Dim result As xcms2() = xic_deco_task.extractAlignedPeaks(samples_xic,
+            rtRange:=New DoubleRange(pars.rt_range),
+            baseline:=pars.baseline,
+            joint:=pars.joint, xic_align:=True,
+            rt_shifts:=shifts)
+
+        Return (result, shifts.ToArray)
     End Function
 
 End Class
