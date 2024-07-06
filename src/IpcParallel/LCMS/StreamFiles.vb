d75bde3d899d9c4b381ef3283c39c2e57539a1f2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-06 14:50:37 +0800

    implements of the file io

--- src/IpcParallel/LCMS/StreamFiles.vb
+++ src/IpcParallel/LCMS/StreamFiles.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 Imports BioNovoGene.Analytical.MassSpectrometry.Math
 Imports Darwinism.HPC.Parallel
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Public Class PeakResultPack : Implements IEmitStream
 
@@ -38,18 +39,41 @@ End Class
 Public Class XicPack : Implements IEmitStream
 
     Public Function BufferInMemory(obj As Object) As Boolean Implements IEmitStream.BufferInMemory
-        Throw New NotImplementedException()
+        Return True
     End Function
 
     Public Function WriteBuffer(obj As Object, file As Stream) As Boolean Implements IEmitStream.WriteBuffer
-        Throw New NotImplementedException()
+        Dim xicdata As FeatureXic = obj
+        Dim bin As New BinaryWriter(file)
+
+        bin.Write(xicdata.samples.Length)
+        bin.Write(xicdata.mz)
+        bin.Write(xicdata.samples.GetJson)
+        bin.Flush()
+
+        SaveXIC.DumpSample(xicdata.peak_groups, file)
+        file.Flush()
+
+        Return True
     End Function
 
     Public Function WriteBuffer(obj As Object) As Stream Implements IEmitStream.WriteBuffer
-        Throw New NotImplementedException()
+        Dim file As New MemoryStream
+        Call WriteBuffer(obj, file)
+        Return file
     End Function
 
     Public Function ReadBuffer(file As Stream) As Object Implements IEmitStream.ReadBuffer
-        Throw New NotImplementedException()
+        Dim bin As New BinaryReader(file)
+        Dim size As Integer = bin.ReadInt32
+        Dim mz As Double = bin.ReadDouble
+        Dim samples As String() = bin.ReadString.LoadJSON(Of String())
+        Dim xicdata As MzGroup() = SaveXIC.ReadSample(file).ToArray
+
+        Return New FeatureXic With {
+            .mz = mz,
+            .samples = samples,
+            .peak_groups = xicdata
+        }
     End Function
 End Class
