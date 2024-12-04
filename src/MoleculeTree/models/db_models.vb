22faffcde1723c830c4c06dde958f149babed263
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-04 14:42:15 +0800

    update database schema model files

--- src/MoleculeTree/models/db_models.vb
+++ src/MoleculeTree/models/db_models.vb
@@ -7,12 +7,16 @@ Namespace treeModel
 Public MustInherit Class db_models : Inherits IDatabase
 Protected ReadOnly m_atoms As Model
 Protected ReadOnly m_graph As Model
+Protected ReadOnly m_molecule_atoms As Model
+Protected ReadOnly m_molecules As Model
 Protected ReadOnly m_tree As Model
 Protected Sub New(mysqli As ConnectionUri)
 Call MyBase.New(mysqli)
 
 Me.m_atoms = model(Of atoms)()
 Me.m_graph = model(Of graph)()
+Me.m_molecule_atoms = model(Of molecule_atoms)()
+Me.m_molecules = model(Of molecules)()
 Me.m_tree = model(Of tree)()
 End Sub
 End Class
