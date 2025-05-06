ecfc0491fddafbeadcad1bb276087ae0a02f38fd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-06 11:29:56 +0800

    Update create_models.cmd

--- src/mysql/create_models.cmd
+++ src/mysql/create_models.cmd
@@ -5,3 +5,8 @@ set source="./molecule_tree.sql"
 set out="../MoleculeTree/models"
 
 %reflector% --reflects /sql %source% -o %out% /namespace treeModel --language visualbasic /split 
+
+set source="./sample_pool.sql"
+set out="../spectrumPool/mysql/models"
+
+%reflector% --reflects /sql %source% -o %out% /namespace clusterModels --language visualbasic /split 
