3e7616c88921d5b9ecebd4db5c51e7a4bad07221
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-06 16:02:39 +0800

    export api for create/open data sperctrum clustering model

--- src/hpc/MoleculeNetworking.vb
+++ src/hpc/MoleculeNetworking.vb
@@ -1,4 +1,7 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports spectrumPool
 
@@ -9,4 +12,49 @@ Imports spectrumPool
 <RTypeExport("sample_pool", GetType(dataPool))>
 Module MoleculeNetworking
 
+    ''' <summary>
+    ''' open the spectrum pool from a given resource link
+    ''' </summary>
+    ''' <param name="model_id">
+    ''' the model id, this parameter works for open the model in the cloud service
+    ''' </param>
+    ''' <param name="score_overrides">
+    ''' WARNING: this optional parameter will overrides the mode score 
+    ''' level when this parameter has a positive numeric value in 
+    ''' range ``(0,1]``. it is dangers to overrides the score parameter
+    ''' in the exists model.
+    ''' </param>
+    ''' <returns></returns>
+    <ExportAPI("openMysqlPool")>
+    Public Function openPool(repo As dataPool,
+                             Optional model_id As String = Nothing,
+                             Optional score_overrides As Double? = Nothing,
+                             Optional env As Environment = Nothing) As BioNovoGene.BioDeep.MassSpectrometry.MoleculeNetworking.PoolData.SpectrumPool
+
+        If score_overrides IsNot Nothing AndAlso
+            score_overrides > 0 AndAlso
+            score_overrides < 1 Then
+
+            Call env.AddMessage($"NOTICE: the score level of the spectrum graph model has been overrides to {score_overrides}!", MSG_TYPES.WRN)
+        End If
+
+        Return repo.Open(model_id:=model_id, score:=score_overrides)
+    End Function
+
+    ''' <summary>
+    ''' create a new spectrum clustering data pool
+    ''' </summary>
+    ''' <param name="level"></param>
+    ''' <param name="split">hex, max=15</param>
+    ''' <returns></returns>
+    <ExportAPI("createMysqlPool")>
+    <RApiReturn(GetType(BioNovoGene.BioDeep.MassSpectrometry.MoleculeNetworking.PoolData.SpectrumPool))>
+    Public Function createPool(repo As dataPool,
+                               Optional level As Double = 0.9,
+                               Optional split As Integer = 9,
+                               Optional name As String = "no_named",
+                               Optional desc As String = "no_information") As Object
+
+        Return repo.Create(level, split:=split, name:=name, desc:=desc)
+    End Function
 End Module
