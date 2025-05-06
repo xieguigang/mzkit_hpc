8f6e46b5cc115603ab6a7067fab219cc01c69e52
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-06 11:34:10 +0800

    export mysql model

--- src/spectrumPool/mysql/dataPool.vb
+++ src/spectrumPool/mysql/dataPool.vb
@@ -1,6 +1,14 @@
-﻿Imports Oracle.LinuxCompatibility.MySQL.Uri
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+Imports spectrumPool.clusterModels
 
 Public Class dataPool : Inherits clusterModels.db_models
+
+    Public ReadOnly Property spectrum_pool As TableModel(Of spectrum_pool)
+        Get
+            Return m_spectrum_pool
+        End Get
+    End Property
 
     Public Sub New(mysqli As ConnectionUri)
         MyBase.New(mysqli)
