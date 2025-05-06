1e27e179ec9254923c59719afc0aa39abed14f0d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-06 13:42:40 +0800

    create mysql data driver

---
+++ src/spectrumPool/driver/mysqlFs.vb
@@ -0,0 +1,44 @@
+﻿Imports BioNovoGene.BioDeep.MassSpectrometry.MoleculeNetworking.PoolData
+
+Public Class mysqlFs : Inherits PoolFs
+
+    Public Overrides Sub CommitMetadata(path As String, data As MetadataProxy)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub SetRootId(path As String, id As String)
+        Throw New NotImplementedException()
+    End Sub
+
+    Protected Overrides Sub Close()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Function CheckExists(spectral As BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.PeakMs2) As Boolean
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function GetTreeChilds(path As String) As IEnumerable(Of String)
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function LoadMetadata(path As String) As MetadataProxy
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function LoadMetadata(id As Integer) As MetadataProxy
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function FindRootId(path As String) As String
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function ReadSpectrum(p As Metadata) As BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.PeakMs2
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function WriteSpectrum(spectral As BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.PeakMs2) As Metadata
+        Throw New NotImplementedException()
+    End Function
+End Class
---
+++ src/spectrumPool/driver/mysqlRepository.vb
@@ -0,0 +1,30 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
+Imports BioNovoGene.BioDeep.MassSpectrometry.MoleculeNetworking.PoolData
+
+Public Class mysqlRepository : Inherits MetadataProxy
+
+    Default Public Overrides ReadOnly Property GetById(id As String) As Metadata
+        Get
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property AllClusterMembers As IEnumerable(Of Metadata)
+    Public Overrides ReadOnly Property Depth As Integer
+    Public Overrides ReadOnly Property RootId As String
+
+    Public Overrides Sub Add(id As String, metadata As Metadata)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub Add(id As String, score As Double, align As AlignmentOutput, pval As Double)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub SetRootId(hashcode As String)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Function HasGuid(id As String) As Boolean
+        Throw New NotImplementedException()
+    End Function
+End Class
