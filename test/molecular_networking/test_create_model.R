b31ca64b4545067b8bf1b7ce6200efe4709b6ffc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-06 16:25:28 +0800

    Update test_create_model.R

--- test/molecular_networking/test_create_model.R
+++ test/molecular_networking/test_create_model.R
@@ -1,11 +1,15 @@
 require(mzkit_hpc);
 require(graphQL);
 
+imports "spectrumPool" from "mzDIA";
+
 let repo = open_mnlink(user = "xieguigang", passwd = 123456, host = "192.168.3.15", port = 3306);
 
 print(repo);
 
-repo |> createMysqlPool(level = 0.9,
+let model = repo |> createMysqlPool(level = 0.9,
                         split = 9,
                         name = "no_named",
-                        desc = "no_information")
+                        desc = "no_information");
+
+print(model |> model_id());
