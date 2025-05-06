d3dadd6dd1d3572764cc266d4718572e81d90195
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-06 11:33:06 +0800

    export database models

---
+++ src/hpc/MoleculeNetworking.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports spectrumPool
+
+''' <summary>
+''' make clustering and networking of the lc-ms ms2 spectrum data
+''' </summary>
+<Package("molecule_networking")>
+<RTypeExport("sample_pool", GetType(dataPool))>
+Module MoleculeNetworking
+
+End Module
---
+++ src/spectrumPool/mysql/dataPool.vb
@@ -0,0 +1,8 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Public Class dataPool : Inherits clusterModels.db_models
+
+    Public Sub New(mysqli As ConnectionUri)
+        MyBase.New(mysqli)
+    End Sub
+End Class
